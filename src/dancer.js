cdf23cc7eacc87448073bf5fd4e8958220092223
MicahWeiss <micahjweiss@gmail.com>
2018-09-24 17:27:32 -0500

    began refactoring to psuedoclassical 2/3 tests passing

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,32 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var Dancer = function(top, left, timeBetweenSteps) { //constructor -- need to change into dancer
+  // use jQuery to create an HTML <span> tag
+  this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  //this.step; // not sure?
+  this.setPosition(top, left); //???
+  
+};
+
+Dancer.prototype.step = function(){
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step, this.timeBetweenSteps);
+}
+Dancer.prototype.setPosition = function(top, left){
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+}
+
+/*
+COPY CODE HERE. PASTE BACK. 
+
 
   var dancer = {};
 
@@ -29,4 +56,7 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   dancer.setPosition(top, left);
 
   return dancer;
-};
+
+
+
+*/
