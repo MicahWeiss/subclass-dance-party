090bb59a601b8d6a22c6f949b78cc2e1eb9c666c
MicahWeiss <micahjweiss@gmail.com>
2018-09-24 19:38:18 -0500

    we got blinkies

--- src/dancer.js
+++ src/dancer.js
@@ -1,17 +1,19 @@
+
+
 // Creates and returns a new dancer object that can step
 var Dancer = function(top, left, timeBetweenSteps) { //constructor -- need to change into dancer
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.timeBetweenSteps = timeBetweenSteps;
-  //this.step; // not sure?
+
   this.setPosition(top, left); //???
-  
+  this.step();
 };
 
 Dancer.prototype.step = function(){
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
 }
 Dancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
@@ -23,6 +25,7 @@ Dancer.prototype.setPosition = function(top, left){
   };
   this.$node.css(styleSettings);
 }
+
 
 /*
 COPY CODE HERE. PASTE BACK. 
